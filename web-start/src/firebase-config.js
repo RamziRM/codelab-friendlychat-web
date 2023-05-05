4555126bc7445a728e887d6fecdef8dbf1203843
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-05-05 18:46:14 -0300

    login - cloud firestore - msg read + display

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -1,19 +1,29 @@
 /**
  * To find your Firebase config object:
- * 
+ *
  * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+    throw new Error(
+      "No Firebase configuration object provided." +
+        "\n" +
+        "Add your web app's configuration object to firebase-config.js"
+    );
   } else {
     return config;
   }
